f1e106f6f3bd7a4778b20a96278ff52aa152469f
Keith Chester <kchester@gmail.com>
2017-03-25 12:10:35 -0500

    feature: HUZZAH switchover, OLED screen, water pump
    Added water pump feature
    
    Got ADABOX 003 from adafruit, so decided to use the new toys.
    
    Switched over to HUZZAH feather.
    
    Added OLED screen w/ buttons for manual trigger and visual update on how
    things are going.

--- garden-mqtt-relay.ino
+++ garden-mqtt-relay.ino
@@ -1,27 +1,75 @@
 /*
-This is an MQTT relay board - the target board was the esp8266 nodemcu board
+This is an MQTT relay board - the target board is the Adafruit Feaether Huzzah ESP8266
+
+I am also adding the OLED featherwing as well for output.
 */
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-#define WIFI_SSID "Your SSID"
-#define WIFI_PASSWORD "Your super secret secure password"
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-#define MQTT_HOST "Host address or IP"
+#define WIFI_SSID "YOUR SSID"
+#define WIFI_PASSWORD "YOUR PASSWORD"
+
+#define MQTT_HOST "MQTT HOST IP"
 #define MQTT_PORT 1883
 
-#define SUB_TOPIC "topic/to/subscribe/to"
+#define LIGHT_SUB_TOPIC "garden/lights"
+#define PUMP_SUB_TOPIC "garden/pump"
 
 //Sometimes the relays are active low, somtimes active high. Mine's low. To make life easier, abstract it
 #define ON 0
 #define OFF 1
 
-//Relay pin
-#define RELAY_PIN 15
+//Relay pins
+#define LIGHT_RELAY_PIN 5
+#define PUMP_RELAY_PIN 6
+
+#define PUMP_DELAY 3000
+
+//OLED PINS
+#define BUTTON_A 0
+#define BUTTON_B 16
+#define BUTTON_C 2
+#define LED      0
 
 WiFiClient wifi;
 PubSubClient client(wifi);
+
+Adafruit_SSD1306 display = Adafruit_SSD1306();
+
+void displayMessage(String msg){
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println(msg);
+  display.println(WiFi.localIP());
+  display.display();
+}
+
+void pumpCycle(){
+  digitalWrite(PUMP_RELAY_PIN, ON);
+  displayMessage("Pump it up!");
+  delay(PUMP_DELAY);
+  digitalWrite(PUMP_RELAY_PIN, OFF);
+}
+
+void turnOnLights(){
+  digitalWrite(LIGHT_RELAY_PIN, ON);
+  displayMessage("Let there be light");
+  delay(2000);
+}
+
+void turnOffLights(){
+  displayMessage("Goodnight, plants");
+  display.display();
+  delay(2000);
+}
 
 void connect_to_wifi() {
   Serial.println();
@@ -40,6 +88,9 @@ void connect_to_wifi() {
   Serial.println("Wifi is connected!");
   Serial.print("My IP address is ");
   Serial.println(WiFi.localIP());
+
+  displayMessage("Connected! My IP is:");
+  delay(2000);
 }
 
 void handleMessage(char* topic, byte* payload, unsigned int length) {
@@ -53,17 +104,23 @@ void handleMessage(char* topic, byte* payload, unsigned int length) {
   }
   Serial.println();
 
-  // Handle a on or off payload messages
-  if (strcmp(command, "off") == 0) {
-    Serial.println("Relay set to off");
-    digitalWrite(RELAY_PIN, OFF);
-  } else if (strcmp(command, "on") == 0) {
-    Serial.println("Relay set to on");
-    digitalWrite(RELAY_PIN, ON);
+  if(strcmp(topic,LIGHT_SUB_TOPIC) == 0){
+    // Handle a on or off payload messages
+    if (strcmp(command, "off") == 0) {
+      Serial.println("Light relay set to off");
+      turnOffLights();
+    } else if (strcmp(command, "on") == 0) {
+      Serial.println("Relay set to on");
+      turnOnLights();
+    }
+  } else if(strcmp(topic, PUMP_SUB_TOPIC) == 0){
+    pumpCycle();
   }
 }
 
 void reconnect() {
+  displayMessage("Connecting to MQTT Server");
+  delay(2000);
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
@@ -71,21 +128,46 @@ void reconnect() {
     // Attempt to connect
     if (client.connect("ESP8266Client")) {
       Serial.println("Connected!");
-      client.subscribe(SUB_TOPIC);
+      client.subscribe(LIGHT_SUB_TOPIC);
+      client.subscribe(PUMP_SUB_TOPIC);
     } else {
       Serial.print("Failed. State=");
       Serial.print(client.state());
       Serial.println(". Trying again in 5 seconds");
+      displayMessage("Failed to connect to MQTT Server. Trying again in 5 seconds... ");
       // Wait 5 seconds before retrying
       delay(5000);
+      displayMessage("Connecting to MQTT Server");
+      delay(2000);
     }
   }
 }
 
 void setup() {
-  pinMode(RELAY_PIN, OUTPUT);
-  digitalWrite(RELAY_PIN, OFF);
+  //pin setup
+  pinMode(LIGHT_RELAY_PIN, OUTPUT);
+  digitalWrite(LIGHT_RELAY_PIN, OFF);
 
+  pinMode(PUMP_RELAY_PIN, OUTPUT);
+  digitalWrite(PUMP_RELAY_PIN, OFF);
+
+  //OLED pins
+  display.display();
+  delay(1000);
+  display.clearDisplay();
+  pinMode(BUTTON_A, INPUT_PULLUP);
+  pinMode(BUTTON_B, INPUT_PULLUP);
+  pinMode(BUTTON_C, INPUT_PULLUP);
+
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.print("Connecting to WiFi...");
+  display.display();
+  
+  //Serial setup
   Serial.begin(9600);
   connect_to_wifi(); 
   client.setServer(MQTT_HOST, MQTT_PORT);
@@ -97,4 +179,11 @@ void loop() {
     reconnect();
   }
   client.loop();
+
+  //Handle button inputs. If it's pressed down, trigger the function!
+  if (! digitalRead(BUTTON_A)) turnOnLights();
+  if (! digitalRead(BUTTON_B)) turnOffLights();
+  if (! digitalRead(BUTTON_C)) pumpCycle();
+
+  displayMessage("<3   Going swell   <3");
 }
